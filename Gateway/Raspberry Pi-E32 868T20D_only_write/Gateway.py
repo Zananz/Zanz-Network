c255d634259e4a143c158f40f725066a10b6d8a0
Zananz <FranzSperling4@gmail.com>
2021-07-25 16:06:41 +0200

    Gateway fixed broken_pipe_error

--- Gateway/Raspberry Pi-E32 868T20D_only_write/Gateway.py
+++ Gateway/Raspberry Pi-E32 868T20D_only_write/Gateway.py
@@ -9,9 +9,6 @@ DISCONNET_MASSAGE = "!DISCONNECT"
 SERVER = "192.168.178.35"
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
-
 ser = serial.Serial( # E32 686T20D is connected via USB
      port='/dev/ttyACM0',
      baudrate = 9600,
@@ -22,7 +19,7 @@ ser = serial.Serial( # E32 686T20D is connected via USB
 )
 
 
-def send(msg): # send a message to the database
+def send(msg, client): # send a message to the database
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
@@ -33,20 +30,32 @@ def send(msg): # send a message to the database
     
     print(f"[MESSAGE SEND] {message}")
 
-try: #to safely disconnect from database in case of an error
+def run():
+    try: #to safely disconnect from database in case of an error
 
-    print("Gateway started...")
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(ADDR)
 
-    while True:
+        print("Gateway started...")
 
-        in_come = ser.readline() # wait till input
-        print(in_come)
-        in_come = in_come.decode()
-        print(in_come)
-        in_come = in_come.replace("\n", "").replace("\r", "")
-        print(in_come)
-        send(in_come)
+        while True:
 
-finally:
-    send(DISCONNET_MASSAGE)
+            in_come = ser.readline() # wait till input
+            print(in_come)
+            in_come = in_come.decode()
+            print(in_come)
+            in_come = in_come.replace("\n", "").replace("\r", "")
+            print(in_come)
+            send(in_come, client)
+
+    except BrokenPipeError:  # most likely caused by a chance of the public Ip
+        print("[BROKEN PIPE] most most likely caused by a chance of the public Ip. Try to fix automatically.")
+        print("[TRY TO RECONNECT IN]:")
+        for i in range(5):
+            print(5 - i)
+            time.sleep(1)
+        run()
+
+    finally:
+        send(DISCONNET_MASSAGE)
     

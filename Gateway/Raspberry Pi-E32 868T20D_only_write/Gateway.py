81c56ceebd172c55496acd4efaf6947ce37fbc7a
Zananz <FranzSperling4@gmail.com>
2021-07-25 16:15:43 +0200

    Gateway fixed broken_pipe_error

--- Gateway/Raspberry Pi-E32 868T20D_only_write/Gateway.py
+++ Gateway/Raspberry Pi-E32 868T20D_only_write/Gateway.py
@@ -2,36 +2,36 @@ import socket
 import serial
 import time
 
-HEADER = 64 # Length of number send to set message length. Don't change must be the same for client and server
+HEADER = 64  # Length of number send to set message length. Don't change must be the same for client and server
 PORT = 52000
 FORMAT = "utf-8"
 DISCONNET_MASSAGE = "!DISCONNECT"
 SERVER = "192.168.178.35"
 ADDR = (SERVER, PORT)
 
-ser = serial.Serial( # E32 686T20D is connected via USB
-     port='/dev/ttyACM0',
-     baudrate = 9600,
-     parity=serial.PARITY_NONE,
-     stopbits=serial.STOPBITS_ONE,
-     bytesize=serial.FIVEBITS,
-     timeout=10000
+ser = serial.Serial(  # E32 686T20D is connected via USB
+    port='/dev/ttyACM0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.FIVEBITS,
+    timeout=10000
 )
 
 
-def send(msg, client): # send a message to the database
+def send(msg, client):  # send a message to the database
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b" " * (HEADER - len(send_length))
-    
+
     client.send(send_length)
     client.send(message)
-    
+
     print(f"[MESSAGE SEND] {message}")
 
-def run():
 
+def run():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
 
@@ -40,8 +40,7 @@ def run():
         print("Gateway started...")
 
         while True:
-
-            in_come = ser.readline() # wait till input
+            in_come = ser.readline()  # wait till input
             print(in_come)
             in_come = in_come.decode()
             print(in_come)
@@ -49,6 +48,22 @@ def run():
             print(in_come)
             send(in_come, client)
 
+
+    except BrokenPipeError:  # most likely caused by a chance of the public Ip
+
+        print("[BROKEN PIPE] most most likely caused by a chance of the public Ip. Try to fix automatically.")
+
+        print("[TRY TO RECONNECT IN]:")
+
+        for i in range(5):
+            print(5 - i)
+
+            time.sleep(1)
+
+        run()
+
     finally:
         send(DISCONNET_MASSAGE, client)
-    
+
+
+run()

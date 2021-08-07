43f88b280766322fcfa1dd38696e9873da77d541
Franz Sperling <48384950+Zananz@users.noreply.github.com>
2021-08-07 12:07:36 +0200

    Update reboot_4am.py

--- Reboot/Raspberry Pi OS/reboot_4am.py
+++ Reboot/Raspberry Pi OS/reboot_4am.py
@@ -6,6 +6,6 @@ time.sleep(60)
 
 while True:
     time_now = str(datetime.datetime.now().time()).split(":")
-    if time_now[0] == "4" and time_now[1] == "00":
+    if time_now[0] == "04" and time_now[1] == "00":
         os.system("sudo reboot")
     time.sleep(58)
